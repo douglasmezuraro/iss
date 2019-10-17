a19087ae95cb30f4bba32f15c664c3706d7ae9a1
Douglas Mezuraro <douglasmez@gmail.com>
2019-10-17 10:52:09 -0300

    Iniciado implementação do carrinho de compras
    * Criado entidade
    * Criado controlador com métodos básicos
    * Criado view inicial do carrinho (que lista os produtos e a quantidade)

---
+++ PSS/PSS/Controllers/CartController.cs
@@ -0,0 +1,86 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using PSS.Models;
+using SGCO.Context;
+
+namespace PSS.Controllers
+{
+    public class CartController : Controller
+    {
+        private Context db = new Context();
+
+        public ActionResult Index()
+        {
+            if (Session["Cart"] == null)
+            {
+                Session["Cart"] = new Cart();
+            }
+
+            var cart = (Cart)Session["Cart"];
+
+            return View(cart.Items.ToList());
+        }
+
+        public ActionResult AddToCart(Item item)
+        {
+            if (Session["Cart"] == null)
+            {
+                Session["Cart"] = new Cart();
+            }
+
+            item.Product = db.Products.Find(item.ProductId);
+            var cart = (Cart)Session["Cart"];
+            cart.Items.Add(item);
+
+            return RedirectToAction("Index", "Products");
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ PSS/PSS/Models/Cart.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace PSS.Models
+{
+    public class Cart
+    {
+        [DisplayName("Items")]
+        public ICollection<Item> Items { get; set; } = new List<Item>();
+    }
+}
