90ab9a5d5c50b7f5d707f3d4981f91d97ba55e4a
Douglas Mezuraro <douglasmez@gmail.com>
2019-10-22 17:49:25 -0300

    Finalizado CRUD de estado

---
+++ PSS/PSS/Controllers/StatesController.cs
@@ -0,0 +1,131 @@
+﻿using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using PSS.Models;
+using SGCO.Context;
+
+namespace PSS.Controllers
+{
+    public class StatesController : Controller
+    {
+        private Context db = new Context();
+
+        public ActionResult Index()
+        {
+            var states = db.States.Include(s => s.Country).Where(s => s.IsActive);
+            return View(states.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            State state = db.States.Find(id);
+            if (state == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(state);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.CountryId = new SelectList(db.Countries, "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(State state)
+        {
+            if (ModelState.IsValid)
+            {
+                db.States.Add(state);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CountryId = new SelectList(db.Countries, "Id", "Name", state.CountryId);
+
+            return View(state);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            State state = db.States.Find(id);
+            if (state == null)
+            {
+                return HttpNotFound();
+            }
+
+            ViewBag.CountryId = new SelectList(db.Countries, "Id", "Name", state.CountryId);
+
+            return View(state);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(State state)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(state).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CountryId = new SelectList(db.Countries, "Id", "Name", state.CountryId);
+
+            return View(state);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            State state = db.States.Find(id);
+            if (state == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(state);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            State state = db.States.Find(id);
+            state.IsActive = false;
+            db.Entry(state).State = EntityState.Modified;
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
