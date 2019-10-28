220f2831e3776305e8527ec504c7dfb44bdc52b1
Douglas Mezuraro <douglasmez@gmail.com>
2019-10-27 23:33:40 -0300

    Atualizado projeto do astah

--- PSS/PSS/Context/Context.cs
+++ PSS/PSS/Context/Context.cs
@@ -1,4 +1,4 @@
-﻿using System.Data.Entity;
+﻿    using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
 using PSS.Models;
 
@@ -6,21 +6,21 @@ namespace SGCO.Context
 {
     public class Context : DbContext
     {
-        public DbSet<Country> Countries { get; set; }
-        public DbSet<State> States { get; set; }
-        public DbSet<City> Cities { get; set; }        
         public DbSet<Category> Categories { get; set; }
-        public DbSet<User> Users { get; set; }
-        public DbSet<Unit> Units { get; set; }
-        public DbSet<Provider> Providers { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Installment> Installments { get; set; }
+        public DbSet<Item> Items { get; set; }
         public DbSet<Manufacturer> Manufacturers { get; set; }
         public DbSet<Product> Products { get; set; }
-        public DbSet<Item> Items { get; set; }
-        public DbSet<Installment> Installments { get; set; }
-        public DbSet<SaleOrderFreight> SaleOrderFreights { get; set; }
+        public DbSet<Provider> Providers { get; set; }
+        public DbSet<PurchaseOrder> PurchaseOrders { get; set; }
         public DbSet<PurchaseOrderFreight> PurchaseOrderFreights { get; set; }
         public DbSet<SaleOrder> SaleOrders { get; set; }
-        public DbSet<PurchaseOrder> PurchaseOrders { get; set; }
+        public DbSet<SaleOrderFreight> SaleOrderFreights { get; set; }
+        public DbSet<State> States { get; set; }
+        public DbSet<Unit> Units { get; set; }
+        public DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
