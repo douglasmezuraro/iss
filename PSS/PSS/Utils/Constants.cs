02f7f58e381e76471c2f0ce4e459e0bd688fba1a
Douglas Mezuraro <douglasmez@gmail.com>
2019-10-31 17:23:14 -0300

    Unificado namespace de constantes

---
+++ PSS/PSS/Utils/Constants.cs
@@ -0,0 +1,45 @@
+﻿namespace PSS.Utils.Constants
+{
+    public static class General
+    {
+        public const byte TEXT_LENGTH = 100;
+        public const byte ABREVIATION_LENGTH = 2;
+        public const byte CEP_LENGTH = 9;
+        public const byte CPF_LENGTH = 14;
+        public const byte CNPJ_LENGTH = 19;
+        public const byte PASSWORD_LENGTH = 16;
+        public const string REAL_VALUE_MASK = "{0:n2}";
+    }
+
+    public static class View
+    {
+        public const string Create = "Cadastrar";
+        public const string Delete = "Deletar";
+        public const string Details = "Detalhes";
+        public const string Edit = "Editar";
+        public const string Index = "Início";
+        public const string Cancel = "Cancelar";
+        public const string Return = "Devolver";
+        public const string Error = "Erro";
+        public const string BackToList = "Voltar";
+        public const string ConfirmDelete = "Você tem certeza que deseja deletar esse registro?";
+        public const string Login = "Login";
+        public const string AddToCart = "Comprar";
+    }
+
+    public static class Action
+    {
+        public const string Create = "Create";
+        public const string Delete = "Delete";
+        public const string Details = "Details";
+        public const string Edit = "Edit";
+        public const string Index = "Index";
+        public const string Cancel = "Cancel";
+        public const string Return = "Return";
+        public const string Error = "Error";
+        public const string BackToList = "BackToList";
+        public const string ConfirmDelete = "ConfirmDelete";
+        public const string Login = "Login";
+        public const string AddToCart = "AddToCart";
+    }
+}
