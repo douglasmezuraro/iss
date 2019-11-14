f0687399ce207a943b41b7b869d1d9ef290c2ef2
Douglas Mezuraro <douglasmez@gmail.com>
2019-11-13 22:02:07 -0300

    Criado classe extensora para pegar a descrição de enums

---
+++ PSS/PSS/Utils/Extensions/EnumExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace PSS.Utils.Extensions
+{
+    // reference: http://jake.burgy.me/tidbits/2016/03/10/get-display-name-for-enum-value.html
+    public static class EnumExtension
+    {
+        public static string EnumDisplayFor(this Enum value)
+        {
+            return value.GetType()
+                        .GetMember(value.ToString())
+                        .FirstOrDefault()
+                        ?.GetCustomAttribute<DisplayAttribute>(false)
+                        ?.Name
+                        ?? value.ToString();
+        }
+    }
+}
