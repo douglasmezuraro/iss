e1348b7a79b6723141b60ac14f94147bae2f945f
Douglas Mezuraro <douglasmez@gmail.com>
2020-01-04 17:58:13 -0300

    Criado classes de teste dos atributos de validações

---
+++ PSS/UnitTest/Utils/Attributes/Validation/CNPJAttributeTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PSS.Utils.Attributes.Validation;
+
+namespace UnitTest.Utils.Attributes.Validation
+{
+    [TestClass]
+    public class CNPJAttributeTest
+    {
+        private readonly CNPJAttribute _attribute = new CNPJAttribute();
+
+        [TestMethod]
+        public void TestEmptyCNPJ()
+        {
+            Assert.IsFalse(_attribute.IsValid(""));
+        }
+
+        [TestMethod]
+        public void TestFormatedEmptyCNPJ()
+        {
+            Assert.IsFalse(_attribute.IsValid("  .   .   /    -  "));
+        }
+
+        [TestMethod]
+        public void TestUnformatedCNPJ()
+        {
+            Assert.IsFalse(_attribute.IsValid("75170583000106"));
+        }
+
+        [TestMethod]
+        public void TestValidCNPJ()
+        {
+            Assert.IsTrue(_attribute.IsValid("75.170.583/0001-06"));
+        }
+    }
+}
---
+++ PSS/UnitTest/Utils/Attributes/Validation/CPFAttributeTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PSS.Utils.Attributes.Validation;
+
+namespace UnitTest.Utils.Attributes.Validation
+{
+    [TestClass]
+    public class CPFAttributeTest
+    {
+        private readonly CPFAttribute _attribute = new CPFAttribute();
+
+        [TestMethod]
+        public void TestEmptyCPF()
+        {
+            Assert.IsFalse(_attribute.IsValid(""));
+        }
+
+        [TestMethod]
+        public void TestFormatedEmptyCPF()
+        {
+            Assert.IsFalse(_attribute.IsValid("   .   .   -  "));
+        }
+
+        [TestMethod]
+        public void TestUnformatedCPF()
+        {
+            Assert.IsFalse(_attribute.IsValid("08698570020"));
+        }
+
+        [TestMethod]
+        public void TestValidCPF()
+        {
+            Assert.IsTrue(_attribute.IsValid("086.985.700-20"));
+        }
+    }
+}
